d547a28135ce295ed3d5f440c2e4661f9c6f3819
mike <mpakleung@gmail.com>
2019-08-06 20:01:14 -0400

    Add logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,10 +9,17 @@ const Header = ({ siteTitle }) => (
     <div className="wave1"></div>
     <div className="wave2"></div>
     <div className="wave3"></div>
-    <nav   className="navbar" role="navigation" aria-label="main navigation">
+    <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
 
-            <Link className="navbar-item" to="/"><h1 className="title">{ siteTitle }</h1></Link>
+            <Link className="navbar-item" to="/">
+              <div className="logo">
+                  <div className="triangle t1"></div>
+                  <div className="triangle t2"></div>
+                  <div className="rectangle"></div>
+              </div>
+              <h1 className="title">{ siteTitle }</h1>
+            </Link>
 
             <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
