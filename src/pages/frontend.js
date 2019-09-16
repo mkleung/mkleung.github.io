564b93f99c41809a16b171ab466f310eaca0f69c
mike <mpakleung@gmail.com>
2019-09-16 15:53:20 -0400

    Redesign the frontend page

--- src/pages/frontend.js
+++ src/pages/frontend.js
@@ -9,40 +9,17 @@ import * as frontendData from "../assets/data/frontend.json";
 class FrontEndPage extends React.Component {
       constructor(props) {
             super(props);
-
             this.state = {
-                  pageNumber: 6
+                  penNumber: 1
             }
       }
 
-      increment = () => {
-            this.setState({ pageNumber: this.state.pageNumber + 6 });
-      }
-
-      showAll = (size) => {
-            this.setState({ pageNumber: size })
-      }
-
-      closeAll = () => {
-            this.setState({ pageNumber: 6 })
+      selectPen = (index) => {
+            this.setState({
+                  penNumber: index
+            })
       }
       render() {
-
-            let incrementButton, showAllButton, closeAllButton;
-            if (this.state.pageNumber < frontendData.projects.length) {
-                  incrementButton = <button className="button is-primary is-rounded greenButton"
-                        onClick={() => this.increment()}
-                  >Load more</button>;
-
-                  showAllButton = <button className="button is-primary is-rounded greenButton"
-                        onClick={() => this.showAll(frontendData.length)}
-                  >Show All</button>;
-            }
-            else {
-                  closeAllButton = <button className="button is-primary is-rounded greenButton"
-                        onClick={() => this.closeAll()}
-                  >Close All</button>;
-            }
 
             return (
                   <Layout navLocation="project">
@@ -53,34 +30,43 @@ class FrontEndPage extends React.Component {
                                           <div className="column is-6"><ProjectsImage /></div>
                                     </div>
                                     <div className="columns is-vcentered is-multiline">
-                                          <div className="column is-6">
+                                          <div className="column is-12">
                                                 <h2 className="has-text-weight-bold">100 Days of CSS</h2>
                                                 <p>Here I try to challenge myself on <a target="_blank" rel="noopener noreferrer" href="https://100dayscss.com/">https://100dayscss.com/</a></p>
+                                                <br />
                                           </div>
-                                          <div className="column is-6 has-text-right">
-                                                {showAllButton}
-                                                {closeAllButton}
-                                          </div>
-                                          {frontendData.projects.slice(0, this.state.pageNumber).map(item => {
-                                                return (
-                                                      <div key={item.id} className="column is-6 css-1">
-                                                            <Codepen
-                                                                  hash={item.hash}
-                                                                  user="mpakleung"
-                                                                  defaultTab="result"
-                                                                  loader={() => <div>Loading...</div>}
-                                                            />
-                                                      </div>
-                                                )
-                                          })}
+                                          <div className="column is-6 pen-container">
+                                                <div className="pen-titles">
+                                                      {frontendData.projects.map((item, index) => {
+                                                            let activeClass = this.state.penNumber === item.id ? "active pen-title text-left" : "text-left pen-title";
+                                                            return (
+                                                                  <div onClick={() => this.selectPen(item.id)} className={activeClass} key={index}>{item.title}</div>
+                                                            )
+                                                      })}
+                                                </div>
 
-                                          <div className="column is-12 has-text-centered	">
-                                                {incrementButton}
-                                          </div>
-
-                                          <div className="column is-12 has-text-centered">
 
                                           </div>
+                                          <div className="column is-6 has-text-centered pen-container">
+                                                <div className="pens">
+                                                      {frontendData.projects.map((item, index) => {
+                                                            let penClass = this.state.penNumber === item.id ? "active pen" : "pen";
+                                                            return (
+                                                                  <div key={index} className={penClass}>
+                                                                        <Codepen
+                                                                              hash={item.hash}
+                                                                              user="mpakleung"
+                                                                              defaultTab="result"
+                                                                              loader={() => <div>Loading...</div>}
+                                                                        />
+                                                                  </div>
+
+                                                            )
+                                                      })}
+                                                </div>
+                                          </div>
+
+
                                     </div>
                               </div>
                         </div>
