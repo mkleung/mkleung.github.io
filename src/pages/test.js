1ed6c9db66988ae354d97eff180300e9412923a3
mike <mpakleung@gmail.com>
2019-09-16 01:44:52 -0400

    Change font

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,19 +1,45 @@
 import React from "react"
 import Layout from "../components/layout"
+import * as frontendData from "../assets/data/frontend.json";
+import Codepen from "react-codepen-embed";
+const Test = () => (
+    < Layout >
+        <div className="hero-body">
+            <div className="container">
+                <div className="columns is-vcentered is-multiline main-feature">
+                    <div className="column is-12 has-text-centered">
+                        {frontendData.projects.length}
 
-class Test extends React.Component {
-    render() {
-        return (
-            <Layout>
-                <div className="hero-body">
-                    <div className="container">
-                        <div className="columns is-vcentered is-multiline main-feature">
-                            Test
+                        <div class="pens">
+
+                            <div className="codepens">
+                                {frontendData.projects.map((item, index) => {
+                                    let penClass = "pen-" + index;
+                                    let penClass2 = "pen " + penClass;
+                                    let inputClass = "input-" + index;
+                                    return (
+                                        <div key={index}>
+                                            <input className={inputClass} id={penClass} type="radio" name="dashboard" />
+                                            <div className={penClass2} >
+                                                <Codepen
+                                                    hash={item.hash}
+                                                    user="mpakleung"
+                                                    defaultTab="result"
+                                                    loader={() => <div>Loading...</div>}
+                                                />
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </Layout>
-        )
-    }
-}
+            </div>
+        </div>
+    </Layout >
+)
 export default Test
+
+
+
